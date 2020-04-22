11c691657310c69d9fc6255b83b044e341fb4a97
kiriakivel <kiriaki.vel@gmail.com>
2020-04-22 18:26:23 +0300

    Add parameterized tests for factorial

---
+++ seip2020_practical_assignments/unittesting/src/test/java/math/MyMathTest_factorial_Parameterized.java
@@ -0,0 +1,35 @@
+package math;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameter;
+import org.junit.runners.Parameterized.Parameters;
+import org.junit.runner.RunWith;
+import org.junit.Test;
+import org.junit.Assert;
+
+@RunWith(Parameterized.class)
+public class MyMathTest_factorial_Parameterized {
+
+	@Parameter (value = 0)
+	public int number;
+
+	@Parameter (value = 1)
+	public int result;
+
+	MyMath math = new MyMath();
+
+	@Parameters
+	public static Collection<Object[]> data() {
+		Object[][] data = new Object[][]{{0,1},{1,1},{2,2},{3,6},{12,479001600}};
+		return Arrays.asList(data);
+	}
+
+	@Test
+	public void test_factorial() {
+		Assert.assertEquals(result, math.factorial(number))
+	}
+	
+}
